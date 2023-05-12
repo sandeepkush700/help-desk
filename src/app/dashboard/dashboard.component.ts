4aa14c69b5c394cfaefce787872f850e22cbec1d
sandeep <Surya.kant@sapple.co.in>
2023-05-12 18:06:16 +0530

    required add

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,52 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms'
-import { HelpdeskCommonService} from '../services/helpdesk-common.service'
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HelpdeskCommonService } from '../services/helpdesk-common.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  registrationform: FormGroup | any;
+  uploadedImage: File | undefined;
 
-  registrationform: FormGroup | any
-  uploadedImage: File |undefined
-
-  constructor(private commonservice: HelpdeskCommonService) { }
+  constructor(private commonservice: HelpdeskCommonService) {}
 
   ngOnInit(): void {
-    this.createForm()
+    this.createForm();
   }
 
   createForm() {
-
     this.registrationform = new FormGroup({
-      lname: new FormControl(''),
-      email: new FormControl(''),
-      mobile: new FormControl(''),
-      department: new FormControl(''),
-      query: new FormControl(''),
-      file: new FormControl('')
-
-    }) 
+      lname: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      mobile: new FormControl('', Validators.required),
+      department: new FormControl('', Validators.required),
+      query: new FormControl('', Validators.required),
+      file: new FormControl('', Validators.required),
+    });
   }
 
-  public onImageUpload(event:any) {
+  public onImageUpload(event: any) {
     this.uploadedImage = event.target.files[0];
-    console.log(this.uploadedImage)
+    console.log(this.uploadedImage);
   }
 
   onSubmit() {
-    console.log(this.registrationform.value)
+    if (this.registrationform.invalid) {
+      return;
+    }
+    console.log(this.registrationform.value);
     let lname = this.registrationform.value.lname;
     let email = this.registrationform.value.email;
     let mobile = this.registrationform.value.mobile;
     let department = this.registrationform.value.department;
     let query = this.registrationform.value.query;
     // let file = this.registrationform.value.file;
-
-    this.commonservice.getUserFormData(lname,email,mobile,department,query,this.uploadedImage)
+    console.log(this.registrationform);
+    this.commonservice.getUserFormData(
+      lname,
+      email,
+      mobile,
+      department,
+      query,
+      this.uploadedImage
+    );
   }
 }
-
-
